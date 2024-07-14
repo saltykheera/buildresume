ed2c6e2fe500b8f6057f393207d0938630fdf8f6
tatyabicchu <vishal.kumar-ug23@nsut.ac.in>
2024-07-14 17:06:36 +0530

    comiit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
-<script setup></script>
-
 <template>
   <main>
-    <h1>home </h1>
+    <h1>Build your resume here</h1>
+    <v-btn @click="navigateToBuildResume">Build Resume</v-btn>
   </main>
 </template>
+
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const navigateToBuildResume = () => {
+  router.push("/buildresume");
+};
+</script>
